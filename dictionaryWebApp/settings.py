68f73cff23b3a2f3f71daf846493ff054bfd007b
RAJAT PRAKASH <lamecodercse@gmail.com>
2021-12-26 01:22:00 +0530

    just a simple error

--- dictionaryWebApp/settings.py
+++ dictionaryWebApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .local_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,24 +78,24 @@ WSGI_APPLICATION = 'dictionaryWebApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-# https://console.clever-cloud.com/users/me/addons/addon_b383dd81-9fe7-4712-baa2-fa79f7a07226/ migrations 
 # DATABASES = {
-#     'default':
-#     {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'bdgh1vkjd1g9spielazq',
-#         'USER': 'udnxl7tmdw8cbihr',
-#         'PASSWORD': 'J3Is99v2hmwdPrh6wQjM',
-#         'HOST': 'bdgh1vkjd1g9spielazq-mysql.services.clever-cloud.com',
-#         'PORT': '3306'
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# https://console.clever-cloud.com/users/me/addons/addon_b383dd81-9fe7-4712-baa2-fa79f7a07226/ migrations 
+DATABASES = {
+    'default':
+    {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bdgh1vkjd1g9spielazq',
+        'USER': 'udnxl7tmdw8cbihr',
+        'PASSWORD': 'J3Is99v2hmwdPrh6wQjM',
+        'HOST': 'bdgh1vkjd1g9spielazq-mysql.services.clever-cloud.com',
+        'PORT': '3306'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
